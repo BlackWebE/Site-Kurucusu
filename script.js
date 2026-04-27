dfa2b178b329a155beb7d2c94e385cd9a57b2e4e
BlackWordE <75865410+BlackWebE@users.noreply.github.com>
2026-04-27 04:41:52 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -17,3 +17,40 @@ fetch("games.json")
       container.appendChild(card);
     });
   });
+
+let allGames = [];
+
+fetch("games.json")
+  .then(res => res.json())
+  .then(data => {
+    allGames = data;
+    renderGames(allGames);
+  });
+
+function renderGames(games) {
+  const container = document.getElementById("gameList");
+  container.innerHTML = "";
+
+  games.forEach(game => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h3>${game.name}</h3>
+      <a href="${game.url}" target="_blank">
+        <button>Oyna</button>
+      </a>
+    `;
+
+    container.appendChild(card);
+  });
+}
+
+function filterGames(category) {
+  if (category === "all") {
+    renderGames(allGames);
+  } else {
+    const filtered = allGames.filter(g => g.category === category);
+    renderGames(filtered);
+  }
+}
